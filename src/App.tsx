7f83f71110328aa5a3bf8c9846ec05c76a06e3df
rohitgithub016 <rohitbhandari016@gmail.com>
2024-05-12 21:57:07 +0530

    Add

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 import WebApp from "@twa-dev/sdk";
 
 function App() {
   console.log(WebApp);
+
+  const [group, setGroup] = useState("https://t.me/+_RiWhOpbYGg2ZDg1");
+
   const handleClick = () => {
     WebApp?.close();
 
-    WebApp.openTelegramLink("https://t.me/+_RiWhOpbYGg2ZDg1");
-    WebApp?.close();
+    WebApp.openTelegramLink(group);
+    // WebApp?.close();
   };
+
+  const handleChange = (event: any) => {
+    setGroup(event?.target?.value);
+  };
+
   return (
     <div>
       Hello WOrlsd{" "}
       {(window as any).Telegram?.WebApp?.initDataUnsafe?.user?.username}
+      <input onChange={handleChange} />
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
