c752a222d48461947fa4bb622f1e5b71f9ebbc0e
rohitgithub016 <rohitbhandari016@gmail.com>
2024-05-12 21:04:54 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import WebApp from "@twa-dev/sdk";
 
 function App() {
+  console.log(WebApp);
+  const handleClick = () => {
+    WebApp.openTelegramLink("https://t.me/+_RiWhOpbYGg2ZDg1");
+  };
   return (
     <div>
       Hello WOrlsd{" "}
       {(window as any).Telegram?.WebApp?.initDataUnsafe?.user?.username}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
