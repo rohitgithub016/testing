9d879b3fe30ad13f5ff729e22ebc7849300e1851
rohitgithub016 <rohitbhandari016@gmail.com>
2024-05-30 14:43:04 +0530

    Pushed

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,21 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import WebApp from "@twa-dev/sdk";
+import telegramLogin from "telegram-bot-auth";
 
 function App() {
-  console.log(WebApp);
-
-  const [settings, setSettingsClicked] = useState("not CCLIKED");
-
-  useEffect(() => {
-    WebApp.onEvent("settingsButtonClicked", () => {
-      console.log("clicked");
-      setSettingsClicked("clicked");
-    });
-
-    WebApp.SettingsButton.show();
-  }, []);
-
-  const [group, setGroup] = useState("https://t.me/+_RiWhOpbYGg2ZDg1");
-
-  const handleClick = () => {
-    (window as any).Telegram?.WebView?.postEvent("web_app_open_tg_link", () => {
-      WebApp?.close();
-    });
-
-    console.log(WebApp.openTelegramLink(group));
-    WebApp.openTelegramLink(group);
-    // WebApp?.close();
+  const handleClick = async () => {
+    try {
+      const params = {
+        botId: 7023151966, // telergam id of bot
+        botNick: "weekendPac_bot", // telegram username of bot
+        origin: "https://testing-six-gilt.vercel.app/", // domain that is linked to your bot
+        phone: "+917908027118", // phone number to auth
+      };
+      const tgUser = await telegramLogin(params);
+    } catch (error) {
+      console.error("Telegram auth error", error);
+    }
   };
-
-  const handleChange = (event: any) => {
-    setGroup(event?.target?.value);
-  };
-
-  return (
-    <div>
-      Hello WOrlsd{" "}
-      {(window as any).Telegram?.WebApp?.initDataUnsafe?.user?.username}
-      <input onChange={handleChange} />
-      <button onClick={handleClick}>Click Me</button>
-      <p>{WebApp?.version}</p>
-      <p>{(window as any).Telegram?.WebApp?.version ? "Hello" : "World"}</p>
-      <p>{WebApp?.SettingsButton?.isVisible ? "Hello" : "World"} </p>
-      <p>{settings}</p>
-      <p></p>
-    </div>
-  );
+  return <button onClick={handleClick}>Click me</button>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import WebApp from "@twa-dev/sdk";
-
-WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
