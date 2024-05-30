9de80769c0ffea040685e8f8d089966c7bcc05f6
rohitgithub016 <rohitbhandari016@gmail.com>
2024-05-30 14:50:51 +0530

    PUSED

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     try {
       const params = {
         botId: 7023151966, // telergam id of bot
-        botNick: "weekendPac_bot", // telegram username of bot
+        botNick: "weekendPrac", // telegram username of bot
         origin: "https://testing-six-gilt.vercel.app/", // domain that is linked to your bot
         phone: "+917908027118", // phone number to auth
       };
