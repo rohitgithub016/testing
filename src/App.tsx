0e3509597edacf86f7b135a5c68567af151b0e50
rohitgithub016 <rohitbhandari016@gmail.com>
2024-07-24 13:16:39 +0530

    asdjfljasdf

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,59 @@ import { useEffect, useState } from "react";
 
 
 const VisibilityTracker = () => {
-  const getVisi = (data: boolean) => {
-    if(data){
-      return "visible"
-    }
-    return "hidden"
-  }
-  const [isVisible, setIsVisible] = useState<string[]>([getVisi(!document.hidden)]);
+
+  const [isVisible, setIsVisible] = useState<string[]>([]);
   const botUsername = "weekendPac_bot";
   const handleClick = () => {
     WebApp.openTelegramLink(`https://t.me/${botUsername}?startgroup=true`)
   }
 
+  const [value, setValue] = useState(0)
 
-  console.log(isVisible);
-
-  const handleVisibilityChange = () => {
-    setIsVisible(prev => [...prev, `${getVisi(!document.hidden)}${prev.length}`]);
-  };
+  useEffect(()=>{
+    setInterval(()=>{
+      setValue(value => value + 1)
+    }, 1000)
+  },[])
 
   useEffect(() => {
-    document.addEventListener('visibilitychange', handleVisibilityChange);
+    
+      // Handle visibility change
+      document.addEventListener('visibilitychange', function() {
+        if (document.visibilityState === 'hidden') {
+          console.log('Page is hidden');
+          setIsVisible(prev => [...prev, `${prev?.length}_HIDDEN`])
+          // Add your custom logic here
+        } else if (document.visibilityState === 'visible') {
+          console.log('Page is visible');
+          setIsVisible(prev => [...prev, `${prev?.length}_VISIBLE`])
 
-    return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
+          // Add your custom logic here
+        }
+      });
+    
+      // Handle focus and blur events
+      window.addEventListener('focus', function() {
+        console.log('Window is focused');
+        // Add your custom logic here
+        setIsVisible(prev => [...prev, `${prev?.length}_VISIBLE`])
+
+      });
+    
+      window.addEventListener('blur', function() {
+        console.log('Window is blurred');
+        // Add your custom logic here
+        setIsVisible(prev => [...prev, `${prev?.length}_HIDDEN`])
+
+      });
+    
   }, []);
 
   return (
     <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
       {isVisible.map(item => <div>{item}</div>)}
       <button onClick={handleClick}>Click me</button>
+      <div>{value}</div>
     </div>
   );
 };
