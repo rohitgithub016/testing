cbdd169e9cacc9e60f06867340a4bc72c51bd6ce
rohitgithub016 <rohitbhandari016@gmail.com>
2024-07-24 10:48:45 +0530

    Added eruda

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import eruda from "eruda";
+
+eruda.init();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
