31907d35c6e6d2a4b35f226cd8fa0c664b5322e0
rohitgithub016 <rohitbhandari016@gmail.com>
2024-07-24 21:42:15 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import WebApp from '@twa-dev/sdk';
 const VisibilityTracker = () => {
 
   const getVisibility = (data: boolean) => {
-    if(data){
-        return "hidden"
-    } 
+    if (data) {
+      return "hidden"
+    }
     return "visible"
-}
+  }
 
   const [isVisible, setIsVisible] = useState([getVisibility(!document.hidden)]);
-  const botUsername = 'weekendPac_bot';
+  // const botUsername = 'weekendPac_bot';
+  const botUsername = "hubz_dev_v2_bot"
 
-  
+
 
   const handleClick = () => {
     WebApp.openTelegramLink(`https://t.me/${botUsername}?startgroup=true`);
@@ -21,7 +22,7 @@ const VisibilityTracker = () => {
   };
 
   const handleVisibilityChange = () => {
-    setIsVisible(prev =>  [...prev, getVisibility(!document.hidden)]);
+    setIsVisible(prev => [...prev, getVisibility(!document.hidden)]);
   };
 
   useEffect(() => {
@@ -35,29 +36,39 @@ const VisibilityTracker = () => {
   const startVisibilityCheck = () => {
     const intervalId = setInterval(() => {
       if (!document.hidden) {
-        setIsVisible(prev =>  [...prev, getVisibility(true)]);
+        setIsVisible(prev => [...prev, getVisibility(true)]);
         clearInterval(intervalId);
       }
     }, 1000); // Check every second
   };
 
-  const [count, setCount] = useState(1);
   const [todo, setTodo] = useState<string[]>([])
 
+  const options = {
+    method: 'GET',
+    headers: {
+      'accept': 'application/json, text/plain, */*',
+      'accept-language': 'en-US,en;q=0.9',
+      'x-api-key': 'ovIfmVfSIZ2iqiFzuGqrh2mHF6STcqa71DSf9jPJ'
+    }
+  };
 
-  useEffect(()=>{
-    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
+  
+
+  useEffect(() => {
+    fetch(`https://api-dev.hubz.io/api/v0/group?creatorUsername=rohitbhandari016`, options)
       .then(response => response.json())
       .then(json => {
-        setCount(prev => prev + 1);
-        setTodo(prev => [...prev, `${count}___${json?.title}`])
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        //@ts-expect-error
+         setTodo(json.data.map((group) => group?.groupName))
       })
   }, [isVisible])
 
   return (
     <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', overflow: 'scroll' }}>
       <button onClick={handleClick}>Click me</button>
-      {todo?.map(t=><div>{t}</div>)}
+      {todo?.map(t => <div>{t}</div>)}
     </div>
   );
 };
