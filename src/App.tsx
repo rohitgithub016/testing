97ce7d58b96d7c833362ebab0ea544a2b42a0ca3
rohitgithub016 <rohitbhandari016@gmail.com>
2024-07-25 13:36:04 +0530

    Addded temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import WebApp from "@twa-dev/sdk";
 
 const App = () => {
   const botUsername = "hubz_dev_v2_bot";
 
-  const [vState, setVState] = useState("First");
+  const [display, setDisplay] = useState("none")
 
   const handleClick = () => {
     WebApp.openTelegramLink(`https://t.me/${botUsername}?startgroup=true`);
-    WebApp.showAlert("Refresh", () => {
-      console.log("refetch data");
-      setVState("refetch")
-    })
+    setDisplay("block")
   };
 
+  const handleOverLay = () => {
+    setDisplay("none")
+  };
 
+  useEffect(()=>{
+    window.addEventListener("focus", ()=>console.log("focused"))
+    window.addEventListener("blur", ()=>console.log("blur"))
 
+  },[])
 
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <h1>{vState}</h1>
-      <button onClick={handleClick}>Add Group</button>
-    </div>
+    <>
+      <div
+        id="overlay"
+        onClick={handleOverLay}
+        style={{
+          position: "fixed",
+          display: display,
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "rgba(0,0,0,0.5)",
+          zIndex: 2,
+          cursor: "pointer",
+        }}
+      ></div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        {/* <h1>{vState}</h1> */}
+        <button onClick={handleClick}>Add Group</button>
+      </div>
+    </>
   );
 };
 
