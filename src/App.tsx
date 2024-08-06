1f9fcf695406d8501c6d8906dc0cfd8b57b7b9ac
rohitgithub016 <rohitbhandari016@gmail.com>
2024-08-06 14:06:38 +0530

    Added color

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import WebApp from "@twa-dev/sdk";
+import { useEffect } from "react";
 
 const App = () => {
   const botUsername = "hubz_dev_v2_bot";
@@ -7,48 +8,9 @@ const App = () => {
     WebApp.openTelegramLink(`https://t.me/${botUsername}?startgroup=true`);
   };
 
-  WebApp.onEvent("mainButtonClicked", function () {
-    console.log("Main button clicked");
-  });
-
-  WebApp.onEvent("viewportChanged", () => {
-    console.log("viewPort changed");
-  });
-
-  WebApp.onEvent("themeChanged", ()=>{
-    console.log("themeChanged")
-  })
-  WebApp.onEvent("backButtonClicked", ()=>{
-    console.log("backButtonClicked")
-  })
-  WebApp.onEvent("settingsButtonClicked", ()=>{
-    console.log("settingsButtonClicked")
-  })
-
-  WebApp.onEvent("invoiceClosed", ()=>{
-    console.log("invoiceClosed")
-  })
-
-  WebApp.onEvent("popupClosed", ()=>{
-    console.log("popupClosed")
-  })
-
-  WebApp.onEvent("qrTextReceived", ()=>{
-    console.log("qrTextReceived	")
-  })
-
-  WebApp.onEvent("clipboardTextReceived", ()=>{
-    console.log("clipboardTextReceived")
-  })
-
-  WebApp.onEvent("writeAccessRequested", ()=>{
-    console.log("writeAccessRequested")
-  })
-
-  WebApp.onEvent("contactRequested", ()=>{
-    console.log("contactRequested")
-  })
-
+  useEffect(()=>{
+    WebApp.setBackgroundColor("#EFEFF4")
+  }, [])
 
   return (
     <div
