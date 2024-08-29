6e51a8bff00c7abf6b4a69a6fb116f59c1ad851b
rohitgithub016 <rohitbhandari016@gmail.com>
2024-08-29 22:02:51 +0530

    Added changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import { useEffect } from "react";
 const App = () => {
   useEffect(() => {
     // WebApp?.setHeaderColor(WebApp?.themeParams?.bg_color);
-    console.log(WebApp?.themeParams?.bg_color)
+    console.log(WebApp?.themeParams)
     console.log(WebApp?.themeParams?.secondary_bg_color);
     console.log(WebApp?.headerColor)
-    WebApp?.setHeaderColor("#FF5733");
+
+    WebApp?.setHeaderColor("#000000");
 
   }, []);
   return (
