b0b11d3ef5e7fdb4d8c98211a5ef80c98c3a45fd
rohitgithub016 <rohitbhandari016@gmail.com>
2024-08-29 14:14:30 +0530

    Added changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ const App = () => {
     console.log(WebApp?.themeParams?.bg_color)
     console.log(WebApp?.themeParams?.secondary_bg_color);
     console.log(WebApp?.headerColor)
+    WebApp?.setHeaderColor(WebApp?.themeParams?.secondary_bg_color);
 
   }, []);
   return (
