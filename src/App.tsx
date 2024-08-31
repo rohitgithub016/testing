2403a0334c2cee98ac6112785f9ae743a384c15d
rohitgithub016 <rohitbhandari016@gmail.com>
2024-08-31 09:17:52 +0530

    color

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import WebApp from "@twa-dev/sdk";
-import { useEffect, useState } from "react";
-import { HexColorPicker } from "react-colorful";
+
 
 const App = () => {
 
-  const [color, setColor] = useState("#aabbcc");
-
-  useEffect(()=>{
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    //@ts-expect-error
-    WebApp?.setHeaderColor(color);
-  },[color])
-
-  console.log(color);
   return (
     <div
       style={{
@@ -23,7 +12,7 @@ const App = () => {
         overflow: "hidden", // Prevent scrollbars
       }}
     >
-      <HexColorPicker color={color} onChange={setColor} />
+      Hello World
     </div>
   );
 };
