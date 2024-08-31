4c99b3e26f66c125657ac638b766b5bd55973b13
rohitgithub016 <rohitbhandari016@gmail.com>
2024-08-31 09:56:33 +0530

    adsfasdf

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,6 @@ const App = () => {
   useEffect(()=>{
     console.log(WebApp);
     console.log(WebApp?.themeParams)
-    WebApp.setBackgroundColor("#f4f4f5")
-    WebApp.setHeaderColor("#f4f4f5");
-    WebApp.themeParams.text_color = "#FF0000"
-    WebApp.themeParams.bg_color = "#FF0000"
-    WebApp.themeParams.button_color = "#FF0000"
-    WebApp.themeParams.destructive_text_color = "#FF0000"
-    WebApp.themeParams.header_bg_color = "#0000"
-    WebApp.themeParams.hint_color = "#FF0000"
-    WebApp.themeParams.link_color = "#FF0000"
-    WebApp.themeParams.secondary_bg_color = "#FF0000"
-    WebApp.themeParams.section_bg_color = "#FF0000"
-    WebApp.themeParams.section_header_text_color = "#FF0000"
-    WebApp.themeParams.section_separator_color = "#FF0000"
-    WebApp.themeParams.subtitle_text_color = "#FF0000"
-
   },[])
 
   return (
