a71b4cabf4ad87c10b12b225894c8ac3bc868610
rohitgithub016 <rohitbhandari016@gmail.com>
2024-09-02 16:43:12 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,29 @@ const App = () => {
     WebApp?.setBackgroundColor(WebApp?.themeParams?.secondary_bg_color);
   }, []);
 
+  const callApi = async () => {
+    await fetch("https://api-dev.hubz.io/api/v0/subscriber/payment-success", {
+      method: "POST",
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "x-api-key": "ovIfmVfSIZ2iqiFzuGqrh2mHF6STcqa71DSf9jPJ",
+      },
+      body: JSON.stringify({
+        subscriptionId: "66d59415beaf2bf54d935f40",
+        username: "rohitbhandari016",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        WebApp.openTelegramLink(data?.data?.inviteLink);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
   const handleClick = () => {
-    WebApp.openTelegramLink("https://t.me/+OFHuZeYqSB1jNTE9");
+    callApi();
   };
 
   return (
