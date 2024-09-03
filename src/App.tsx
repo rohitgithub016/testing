a1205350ed24a5f94fb1bc4844281b5a14a540c5
rohitgithub016 <rohitbhandari016@gmail.com>
2024-09-03 11:32:29 +0530

    Hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import WebApp from "@twa-dev/sdk";
-import { SetStateAction, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 
 const App = () => {
 
   const  [customlink, setLink] = useState('');
+  const [openLink, setOpen] = useState(false);
+
+  useEffect(()=>{
+    if(openLink){
+      WebApp?.openTelegramLink(customlink);
+    }
+  }, [openLink]);
 
   const callApi = async () => {
     await fetch("https://api-dev.hubz.io/api/v0/subscriber/payment-success", {
@@ -21,7 +28,30 @@ const App = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        WebApp.openTelegramLink(customlink ? customlink : data?.data?.inviteLink);
+        setLink(data?.data?.inviteLink);
+        setOpen(true)
+        // WebApp.openLink(data?.data?.inviteLink);
+      })
+      .catch((error) => console.error("Error:", error));
+  };
+
+  const callApi1 = async () => {
+    await fetch("https://api-dev.hubz.io/api/v0/subscriber/payment-success", {
+      method: "POST",
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/json",
+        "x-api-key": "ovIfmVfSIZ2iqiFzuGqrh2mHF6STcqa71DSf9jPJ",
+      },
+      body: JSON.stringify({
+        subscriptionId: "66d6a021045a39eb97eef588",
+        username: "rohitbhandari016",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+          WebApp.openLink(data?.data?.inviteLink);
         // WebApp.openLink(data?.data?.inviteLink);
       })
       .catch((error) => console.error("Error:", error));
@@ -31,9 +61,9 @@ const App = () => {
     callApi();
   };
 
-  const handleClick1 = () => {
-    WebApp?.openTelegramLink("https://t.me/+OFHuZeYqSB1jNTE9");
-  }
+  // const handleClick1 = () => {
+  //   WebApp?.openTelegramLink("https://t.me/+OFHuZeYqSB1jNTE9");
+  // }
 
   const handleClick2 = () => {
     WebApp?.openLink("https://t.me/+OFHuZeYqSB1jNTE9");
@@ -56,7 +86,7 @@ const App = () => {
     >
       <input onChange={handleInput}/>
       <button onClick={handleClick}>Click Me</button>
-      <button onClick={handleClick1}>Click Me 1</button>
+      <button onClick={callApi1}>Click Me 1</button>
       <button onClick={handleClick2}>Click Me 2</button>
 
     </div>
