7db794aea12df7a883781415cc1574272b80adba
rohitgithub016 <rohitbhandari016@gmail.com>
2024-09-09 13:41:43 +0530

    Hello w

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import WebApp from "@twa-dev/sdk";
-import  { useEffect } from "react";
+import { useEffect } from "react";
+import Carousel from "./Carousel/Carousel";
 
 const App = () => {
   useEffect(() => {
     console.log(WebApp?.initData);
   }, []);
-  return <div>App</div>;
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/Carousel/Carousel.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Carousel as AntdCarousel, Card, Flex } from "antd";
+import "./index.css";
+
+const contentStyle: React.CSSProperties = {
+  margin: 0,
+  height: "160px",
+  color: "#000",
+  lineHeight: "160px",
+  textAlign: "center",
+};
+
+const Carousel: React.FC = () => {
+  const onChange = (currentSlide: number) => {
+    console.log(currentSlide);
+  };
+
+  return (
+    <AntdCarousel afterChange={onChange}>
+      <Flex>
+        <Card style={contentStyle}>
+          <h3>1</h3>
+        </Card>
+      </Flex>
+      <Flex>
+        <Card style={contentStyle}>
+          <h3>2</h3>
+        </Card>
+      </Flex>
+      <Flex>
+        <Card style={contentStyle}>
+          <h3>3</h3>
+        </Card>
+      </Flex>
+      <Flex>
+        <Card style={contentStyle}>
+          <h3>4</h3>
+        </Card>
+      </Flex>
+    </AntdCarousel>
+  );
+};
+
+export default Carousel;
