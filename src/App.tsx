ab36a05096a8b32ec8189f73c5f6999100e63aba
rohitgithub016 <rohitbhandari016@gmail.com>
2024-09-19 15:02:39 +0530

    adsf

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import WebApp from "@twa-dev/sdk";
 const App = () => {
 
   const handleClick = () => {
-    WebApp.openTelegramLink(`https://t.me/weekendPac_bot?startgroup=true`)
+    WebApp.openTelegramLink(`https://t.me/undefined?startgroup=true`)
   }
 
   const handleClick1 = () => {
     WebApp.openTelegramLink(
-      `https://t.me/weekendPac_bot?startgroup=true&admin=manage_chat invite_users change_info post_messages edit_messages delete_messages pin_messages restrict_members`
+      `https://t.me/undefined?startgroup=true&admin=manage_chat invite_users change_info post_messages edit_messages delete_messages pin_messages restrict_members`
 
     )
   }
