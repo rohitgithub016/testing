5ac73d5074c1b2bc867368f663d14faab04fdfdc
rohitgithub016 <rohitbhandari016@gmail.com>
2024-10-01 10:54:27 +0530

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import WebApp from "@twa-dev/sdk";
-import ChainLinkChat from "./Onboarding/ChainLinkChat";
 
 const App = () => {
-  // const handleClick = () => {
-  //   WebApp.openTelegramLink(`https://t.me/undefined?startgroup=true`);
-  // };
+  const handleClick = () => {
+    WebApp.openTelegramLink(`https://t.me/weekendPac_bot?startgroup=true`);
+  };
 
-  // const handleClick1 = () => {
-  //   WebApp.openTelegramLink(
-  //     `https://t.me/undefined?startgroup=true&admin=manage_chat invite_users change_info post_messages edit_messages delete_messages pin_messages restrict_members`
-  //   );
-  // };
-
-  console.log(WebApp.initDataUnsafe?.chat);
-  console.log(WebApp.initDataUnsafe);
-  console.log(WebApp?.initData);
-  console.log(WebApp?.initDataUnsafe);
-
-  return <ChainLinkChat />;
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <button onClick={handleClick}>Open</button>
+    </div>
+  );
 };
 
 export default App;
