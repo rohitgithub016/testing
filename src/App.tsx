cd38d50f4b794e1804fd030d3367af15a40578f1
rohitgithub016 <rohitbhandari016@gmail.com>
2024-10-04 14:10:28 +0530

    dsafasdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,7 @@
-import WebApp from "@twa-dev/sdk";
-import { useEffect, useState } from "react";
+import ChainLinkChat from "./Onboarding/ChainLinkChat";
 
 const App = () => {
-  const [text, setText] = useState("");
-  useEffect(()=>{
-    window.addEventListener('focus', () => {
-      console.log("focus")
-      setText("focus");
-    })
-    window.addEventListener('blur', () => {
-      console.log("blur")
-      setText("blur");
-    })
-  },[])
-  const handleClick = () => {
-    const a = WebApp.openTelegramLink(
-      `https://t.me/weekendPac_bot?startgroup=true`
-    );
-    console.log(a);
-  };
-
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        color: 'red',
-        fontSize: '54px'
-      }}
-    >
-      <>{text}</>
-      <button onClick={handleClick}>Open</button>
-    </div>
-  );
+  return <ChainLinkChat />;
 };
 
 export default App;
