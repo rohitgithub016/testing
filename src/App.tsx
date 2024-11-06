90a9cd83ae26634b9005237228338672c7e5dd4b
rohitgithub016 <rohitbhandari016@gmail.com>
2024-11-06 11:14:11 +0530

    contract deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,53 @@
+import {
+  TonConnectButton,
+  useTonAddress,
+  useTonConnectUI
+} from "@tonconnect/ui-react";
 import "./style.css";
+import { Address, beginCell, Cell, toNano } from "ton";
+import { ContractDeployer } from "./contract-deployer";
+import BN from "bn.js";
+import minterHex from "./lib/contracts/jetton-minter.compiled.json";
 
+interface ContractDeployDetails {
+  deployer: Address;
+  value: BN;
+  code: Cell;
+  data: Cell;
+  message?: Cell;
+  dryRun?: boolean;
+}
+
+// Specify gas required for deployment
+export const DEPLOY_GAS = toNano(0.00);
+
+// Main application component for deploying the contract
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <input type="number"></input>
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <h2>Start editing to see some magic happen!</h2>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+  const walletAddress = useTonAddress();
+  const [tonConnectUI] = useTonConnectUI();
 
-      <div className="fix-bottom-button">I am fix at bottom</div>
+  async function deployContract() {
+    const ownerAddress = Address.parse(walletAddress);
+
+    const deployParams: ContractDeployDetails = {
+      deployer: ownerAddress,
+      value: DEPLOY_GAS,
+      code: Cell.fromBoc(minterHex.hex)[0], 
+      data: beginCell().storeAddress(ownerAddress).endCell(),
+    };
+
+    const contractAddress = await new ContractDeployer().deployContract(
+      deployParams,
+      tonConnectUI
+    );
+
+    console.log("Contract deployed at:", contractAddress.toString());
+  }
+
+  return (
+    <div>
+      <TonConnectButton />
+      <button onClick={deployContract}>Deploy Contract</button>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <TonConnectUIProvider manifestUrl='https://ipfs.io/ipfs/QmUGoWd9pC3BteB85BoCaMZAVJm8qc2wPYDBe2YsNWoK7R'>
     <App />
+
+    </TonConnectUIProvider>
   </StrictMode>,
 )
